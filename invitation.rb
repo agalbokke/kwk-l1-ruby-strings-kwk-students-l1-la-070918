8d1f87e2133bbe8120671b1606a5062b700da6b7
agalbokke <ashyahgalbokke@gmail.com>
2018-07-10 17:42:40 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,11 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+
+puts "Hi! You've been invited to a party. What's your name?"
+
+guest_name = gets.chomp
+
+
+
+puts "Hi #{guest_name"
